ccc512a3b46c1b7eb3b42c0e050236ec674ebf68
lucasaguilar00 <aaguilar@uade.edu.ar>
2024-08-23 19:41:31 -0300

    Ejercicio 6 listo

--- TP-1/ej6.py
+++ TP-1/ej6.py
@@ -4,7 +4,9 @@ y devuelva como valor de retorno el número que resulte de concatenar ambos
 parámetros. Por ejemplo, si recibe 1234 y 567 debe devolver 1234567. No se permite utilizar 
 facilidades de Python no vistas en clase.
 """
-def concatenar(a : int,b : int):
-    #Concatena dos números enteros cambiandolos a string y luego los retorna como enteros otra vez.
+
+
+def concatenar(a: int, b: int):
+    # Concatena dos números enteros cambiandolos a string y luego los retorna como int.
     c = str(a) + str(b)
     return int(c)
