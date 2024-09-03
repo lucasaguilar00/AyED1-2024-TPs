01a2d3836ae9b6d294c26fff295cabbe2c0f42bc
lucasaguilar00 <aaguilar@uade.edu.ar>
2024-09-03 00:36:25 -0300

    Avance del tp 2 ej 2 al ej 4

---
+++ TP-2/ej2.py
@@ -0,0 +1,81 @@
+"""
+Escribir funciones para:
+a. Generar una lista de N números aleatorios del 1 al 100. El valor de N se ingresa
+a través del teclado.
+b. Recibir una lista como parámetro y devolver True si la misma contiene algún
+elemento repetido. La función no debe modificar la lista.
+c. Recibir una lista como parámetro y devolver una nueva lista con los elementos
+únicos de la lista original, sin importar el orden.
+Combinar estas tres funciones en un mismo programa.
+"""
+import random as rn 
+
+lista = [] #Inicio lista vacía
+
+def cargar_lista(var : list) -> list: #Opción A
+    #Carga lista con la cantidad de indices que desea con números random.
+    while True:
+        try:
+        #Utiliza try para prevenir un posible error que detenga el programa
+            elementos = int(input("Ingrese la cantidad de elementos que desea tener: "))
+            if elementos > 0:
+                for _ in range(elementos):
+                    var.append(rn.randint(1, 100))
+                return var
+            else:
+                print("Introduzca un número mayor a 0.")
+        except ValueError:
+            print("Ingrese un número entero válido")
+
+
+def hay_repetidos(var : list) -> bool: # Opción B
+    #Verifica si hay un elemento repetido en la lista comparando el largo de la lista con un set que no permite datos repetidos.
+    return len(var) != len(set(var))
+
+def lista_unicos(var : list) -> list: #Opción C
+    #Devuelve la lista sin elementos repetidos 
+    return list(set(var))
+
+def opciones():
+    print("Menú de opciones.")
+    print("")
+    print("1-Cargar lista al azar (Elegir la cantidad de elementos).")
+    print("2-Informar si la lista tiene elementos repetidos.")
+    print("3-Obtener la lista con elementos únicos.")
+    print("4-Vaciar lista.")
+    print("0.Salir")
+
+def menu():
+    while True:
+        opciones()
+        print("")
+        op = input("Ingresa una opción.")
+        print("")
+        if op == "1":
+            cargar_lista(lista)
+            print(lista)
+        elif op == "2":
+            rep = hay_repetidos(lista)
+            print(rep)
+            if rep:
+                print("Hay elementos repetidos")
+            else:
+                print("No hay elementos repetidos.")
+        elif op =="3":
+            new_lista = lista_unicos(lista)
+            print("La antigua lista es:")
+            print(lista)
+            print("")
+            print("La nueva lista es:")
+            print(new_lista)
+        elif op == "4":
+            lista.clear()
+            print("La lista ha sido vaciada.")
+        elif op == "0":
+            print("Saliendo del programa.")
+            break
+        else:
+            print("Opción inválida.")
+
+if __name__ == "__main__":
+    menu()
---
+++ TP-2/ej3.py
@@ -0,0 +1,53 @@
+"""
+Crear una lista con los cuadrados de los números entre 1 y N (ambos incluidos),
+donde N se ingresa desde el teclado. Luego se solicita imprimir los últimos 10 valores de la lista. 
+"""
+
+lista = [] #lista vacia
+
+def cargar_lista(var : list) -> list:
+    while True:
+        try:
+        #Utiliza try para prevenir un posible error que detenga el programa
+            elementos = int(input("Ingrese la cantidad de elementos que desea tener: "))
+            if elementos > 1:
+                for i in range(elementos):
+                    cuadrado = (i + 1) ** 2
+                    var.append(cuadrado)
+                return var
+            else: 
+                print("Introduzca un número mayor a 1")
+        except ValueError:
+            print("Ingrese un número entero válido")
+
+def ultimos_elementos(var : list) -> list:
+    #Devuelve los ultimos 10 elementos de la lista, o si tiene menos de 10 la lista entera
+    return var[-10:]
+
+def menu():
+    while True:
+        print("Opciones")
+        print()
+        print("1- Crear lista con los cuadrados del número 1 hasta el..: ")
+        print("2- Imprimir los últimos 10 valores de la lista.")
+        print("3- Vaciar lista.")
+        print("0. Salir.")
+        op = input("Ingrese una opción: ")
+        if op == "1":
+            cargar_lista(lista)
+            print("La lista es la siguiente:")
+            print(lista)
+        elif op == "2":
+            print("Los ultimos 10 valores de la lista son:")
+            print(ultimos_elementos(lista))
+        elif op == "3":
+            lista.clear()
+            print("La lista se ha vaciado.")
+        elif op == "0":
+            print("Saliendo del programa.")
+            break
+        else:
+            print("Opción inválida, intente de nuevo.")
+
+if __name__ == "__main__":
+    menu()
